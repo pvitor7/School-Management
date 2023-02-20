26c9e9cd0304f385468767bbb1f77759de8b7362
paulovitor <pvitor7@hotmail.com>
2023-02-20 16:04:43 -0300

    chore: created route campus/roles

--- campus/views.py
+++ campus/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from .models import Campus
-from .serializers import CampusSerializer
+from .models import Campus, Roles
+from .serializers import CampusSerializer, RolesSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from users.permissions import AdminAuthenticated, OwnerAuthenticated, StudantAuthenticated
@@ -18,4 +18,11 @@ class CampusIdView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [StudantAuthenticated, AdminAuthenticated, OwnerAuthenticated]
     queryset = Campus.objects.all()
     serializer_class = CampusSerializer
+
+
+class RolesListView(generics.ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [AdminAuthenticated]
+    queryset = Roles.objects.all()
+    serializer_class = RolesSerializer
     
