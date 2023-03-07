bd1118641c33c2fc7b1931ac93393b8fda973341
paulovitor <pvitor7@hotmail.com>
2023-03-07 10:54:30 -0300

    choore: edit endpoints classes

--- classes/urls.py
+++ classes/urls.py
@@ -2,6 +2,6 @@ from .views import ClassesIdView, ClassesListCreateView
 from django.urls import path
 
 urlpatterns = [
-    path("campus/courses/<str:course_id>/classes/", ClassesListCreateView.as_view()),
-    path("campus/courses/<str:course_id>/classes/<pk>/", ClassesIdView.as_view()),
+    path("campus/<str:campus_id>/courses/<str:course_id>/classes/", ClassesListCreateView.as_view()),
+    path("campus/<str:campus_id>/courses/<str:course_id>/classes/<pk>/", ClassesIdView.as_view()),
 ]   
