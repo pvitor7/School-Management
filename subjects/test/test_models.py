4f54a450f5dfb8a6fb43f5f9400347f6e75c5df1
paulovitor <pvitor7@hotmail.com>
2023-03-08 18:19:08 -0300

    choore: created test relations

--- subjects/test/test_models.py
+++ subjects/test/test_models.py
@@ -18,7 +18,7 @@ class UserModelTest(TestCase):
         for role in roles:
             Roles.objects.create(title=role['title'], permission=role['permission'], campus=cls.campus)
      
-        role_studant = Roles.objects.get(title="studant", campus=cls.campus)
+        cls.role_studant = Roles.objects.get(title="studant", campus=cls.campus)
      
         cls.studant = User.objects.create_user(**{
             "first_name": "Cristiano",
@@ -28,7 +28,7 @@ class UserModelTest(TestCase):
             "password": "s3nh4s3gur4",
             "cellphone": "11900000088",
             "classe": cls.classe,
-            "role": role_studant
+            "role": cls.role_studant
         })
        
 
@@ -56,12 +56,36 @@ class UserModelTest(TestCase):
         
     
     def test_db_create_subject_studant_with_invalid_user(self):
-        subject = Subjects.objects.create(**{"title": "Matemática", "course": self.course})
+        self.subject = Subjects.objects.create(**{"title": "Matemática", "course": self.course})
         with self.assertRaises(ValueError):
-            SubjectsStudants.objects.create(**{"user": "", "subject": subject, "test_1": None, "test_2": None, "test_3": None, "test_4": None})
+            SubjectsStudants.objects.create(**{"user": "", "subject": self.subject, "test_1": None, "test_2": None, "test_3": None, "test_4": None})
             
     
     def test_db_create_subject_studant_with_invalid_subject(self):
         with self.assertRaises(ValueError):
             SubjectsStudants.objects.create(**{"user": self.studant, "subject": "", "test_1": None, "test_2": None, "test_3": None, "test_4": None})
     
+
+    def teste_db_relations(self):
+        relation_user = User.objects.create_user(**{
+	    	"username": "Gabigol",
+            "password": "s3nh4s3gur4",
+            "first_name": "Gabriel",
+            "last_name": "Barbosa",
+            "email": "gabi.gol@mail.com",
+            "cellphone": "11999999910",
+            "classe": self.classe,
+            "role": self.role_studant
+        })
+        for x in range(10):
+            subject = Subjects.objects.create(**{"title": f"Test {x}", "course": self.course})
+            studant_subject = SubjectsStudants.objects.create(**{"user": relation_user, "subject": subject, "test_1": 0, "test_2": 3, "test_3": 7.5, "test_4": x})
+            self.assertEqual(relation_user, studant_subject.user)
+            self.assertEqual(subject, studant_subject.subject)
+        
+        count_subjects = SubjectsStudants.objects.filter(user=relation_user).count()
+        self.assertEqual(count_subjects, 10)
+            
+                
+                
+            
